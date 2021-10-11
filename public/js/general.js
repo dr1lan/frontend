bb837f45764b997ecea80873910c3aa335a32c0f
ds <s.o.dremova@gmail.com>
2021-10-11 22:42:17 +0300

    WAP-3 Лендинг с функционалом

--- public/js/general.js
+++ public/js/general.js
@@ -1,25 +1,36 @@
 $(document).ready(function () {
     $('#scan_submit').click(function () {
-        $.ajax({
-            url: '/scan',
-            type: 'POST',
-            data: {url: $('#scan_url').val()},
-            dataType: 'json',
-            beforeSend: function () {
-                $('#loader').removeClass('hidden')
-            },
-            success: function (response) {
-                $.redirect('/report', {
-                    report: response.report
-                })
-            },
-            complete: function () {
-                $('#loader').addClass('hidden')
-            },
-            error: function (error) {
-                console.log(error)
-            }
-        });
+        let url = $('#scan_url').val();
+        if (isValidUrl(url)) {
+            $('.t-form__errorbox-bottom').hide();
+            $.ajax({
+                url: '/scan',
+                type: 'POST',
+                data: {url: url},
+                dataType: 'json',
+                beforeSend: function () {
+                    $('#loader').removeClass('hidden')
+                },
+                success: function (response) {
+                    $.redirect('/report', {
+                        report: response.report
+                    })
+                },
+                complete: function () {
+                    $('#loader').addClass('hidden')
+                },
+                error: function (error) {
+                    console.log(error)
+                }
+            });
+        } else {
+            $('.t-form__errorbox-bottom').show();
+        }
     });
 
+    function isValidUrl(url) {
+        let regExp = /(^https?:\/\/)[a-z0-9~_\-\.]+\.[a-z]{2,9}(\/|:|\?[!-~]*)?$/i;
+
+        return regExp.test(url);
+    }
 });
