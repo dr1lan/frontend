a6f5c6d3a60cda173a2d8ec2f9213bcf87e890c3
ds <s.o.dremova@gmail.com>
2021-10-18 15:37:17 +0300

    WAP-6 Доработки по лендингу

--- src/Controller/ScannerController.php
+++ src/Controller/ScannerController.php
@@ -43,4 +43,36 @@ class ScannerController extends AbstractController
 
         return $this->render('landing/report/report.html.twig', ['report' => $report, 'year' => date('Y')]);
     }
+
+    #[Route(path: '/about', name: 'about', methods: 'GET')]
+    public function about(): Response
+    {
+        return $this->render('landing/pages/about.html.twig', [
+            'year' => date('Y'),
+        ]);
+    }
+
+    #[Route(path: '/contacts', name: 'contacts', methods: 'GET')]
+    public function contacts(): Response
+    {
+        return $this->render('landing/pages/contacts.html.twig', [
+            'year' => date('Y'),
+        ]);
+    }
+
+    #[Route(path: '/cookie', name: 'cookie', methods: 'GET')]
+    public function cookie(): Response
+    {
+        return $this->render('landing/pages/cookie.html.twig', [
+            'year' => date('Y'),
+        ]);
+    }
+
+    #[Route(path: '/privacy-policy', name: 'privacy-policy', methods: 'GET')]
+    public function privacyPolicy(): Response
+    {
+        return $this->render('landing/pages/privacy-policy.html.twig', [
+            'year' => date('Y'),
+        ]);
+    }
 }
